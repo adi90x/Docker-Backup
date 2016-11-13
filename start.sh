a3b76097ea4406cf3a9b614419c38968b27d80e6
Adrien M <amaurel90@gmail.com>
2016-11-13 17:22:15 +0100

    Correcting

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #/bin/sh
-tar -czf backup_`date +%Y%m%d_%H%M%S`.tar.gz -C / backup
+tar -czf save/backup_`date +%Y%m%d_%H%M%S`.tar.gz -C / backup
 
 backup=$(ls -p -t backup*.tar.gz | head -1) 
 
 
  if [ -z $PRIV_KEY_NAME ]; then PRIV_KEY_NAME=priv.key ;  fi 
 
-if [$TYPE  = "SCP" ]
+if [ "$TYPE"  = "SCP" ]
  then scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME  $backup $DISTANT_HOST_PATH/$backup
 fi
 
-if [$TYPE  = "FTP" ]
- then  lftp ftp://$DISTANT_HOST_PATH -e "mirror -e -R save $BACKUP_PATH"
+if [ "$TYPE"  = "FTP" ]
+ then  lftp ftp://$DISTANT_HOST_PATH -e "mirror -e -R save $BACKUP_PATH;exit"
 fi
 
 #Should be keep to syncro all  and remove only oldest
-rm $backup
+rm save/$backup
