94b4db5c7ddc8ce41bdcec8f0d88470a3c749949
Adrien M <amaurel90@gmail.com>
2016-11-13 17:00:35 +0100

    First implement of ftp

--- start.sh
+++ start.sh
@@ -3,9 +3,16 @@ tar -czf backup_`date +%Y%m%d_%H%M%S`.tar.gz -C / backup
 
 backup=$(ls -p -t backup*.tar.gz | head -1) 
 
+
  if [ -z $PRIV_KEY_NAME ]; then PRIV_KEY_NAME=priv.key ;  fi 
 
-scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME  $backup $DISTANT_HOST_PATH/$backup
+if [$TYPE  = "SCP" ]
+ then scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME  $backup $DISTANT_HOST_PATH/$backup
+fi
 
+if [$TYPE  = "FTP" ]
+ then  lftp ftp://$DISTANT_HOST_PATH -e "mirror -e -R save $BACKUP_PATH"
+fi
 
+#Should be keep to syncro all  and remove only oldest
 rm $backup
