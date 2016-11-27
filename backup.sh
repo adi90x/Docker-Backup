2f9573b90cf94f1715605b5d6542ed2598138b08
AdrienM <amaurel90@gmail.com>
2016-11-27 21:40:50 +0100

    Working keep X number of backup

--- backup.sh
+++ backup.sh
@@ -7,14 +7,16 @@ backup=$(basename $(ls -p -t /save/backup*.tar.gz | head -1))
 : ${PRIV_KEY_NAME=priv.key}
 : ${KEEP=1}
 
-if [ "$TYPE"  = "SCP" ] then 
+if [ "$TYPE"  = "SCP" ] 
+then 
 sshpass -p "$PASS" scp -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME /save/$backup $DISTANT_HOST_PATH:$BACKUP_PATH/$backup
-sshpass -p "$PASS" ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME  $DISTANT_HOST_PATH "ls -1trpd $BACKUP_PATH/* | head -n -$KEEP | xargs -d '\n' rm -f --"
+sshpass -p "$PASS" ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i /certs/$PRIV_KEY_NAME  $DISTANT_HOST_PATH "ls -1trpd $BACKUP_PATH/* | head -n -$KEEP | xargs rm -f --"
 fi
 
-if [ "$TYPE"  = "FTP" ] then 
+if [ "$TYPE"  = "FTP" ] 
+then 
 lftp ftp://$DISTANT_HOST_PATH -e "mirror -Re /save $BACKUP_PATH/;exit"
 fi
 
 #Should be keep to syncro all and remove only oldest
-ls -1trpd /save/* | head -n -$KEEP | xargs -d '\n' rm -f --
+ls -1trpd /save/* | head -n -$KEEP | xargs rm -f --
